541143b4ee47603ac083d2ca5e8740921fabbbd1
ava <148492613+avaceratops@users.noreply.github.com>
2024-04-29 14:16:12 +0100

    Add /new path to index route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,8 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Mini Message Board', messages });
 });
 
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
 module.exports = router;
