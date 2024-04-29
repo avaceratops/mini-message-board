c7c8eb75e081bb14b9106d306e0c551561baa9dc
ava <148492613+avaceratops@users.noreply.github.com>
2024-04-29 13:33:13 +0100

    Add messages array to index route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hello World!',
+    user: 'Enki',
+    added: new Date(),
+  },
+  {
+    text: "I'm silly",
+    user: 'Apollo',
+    added: new Date(),
+  },
+];
+
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages });
 });
 
 module.exports = router;
